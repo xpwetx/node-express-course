f51a4bdb9e1b73e1437e1d45e8b5e2fa86bf20e6
Cakes <kathryjmiranda@gmail.com>
2025-07-21 20:01:04 -0700

    week 2 answers

--- 01-node-tutorial/03-modules.js
+++ 01-node-tutorial/03-modules.js
@@ -1,9 +1,14 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only share minimum)
-const names = require('./04-names')
-const sayHi = require('./05-utils')
-const data = require('./06-alternative-flavor')
-require('./07-mind-grenade')
+const names = require('./04-names.js')
+const sayHi = require('./05-utils.js')
+const alt = require('./06-alternative-flavor.js')
+require('./07-mind-grenade.js')
 sayHi('susan')
 sayHi(names.john)
 sayHi(names.peter)
+
+// Log alternative exports
+console.log(alt.items);
+console.log(alt.singlePerson);
+
