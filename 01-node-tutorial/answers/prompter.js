eb7d90a8908f4240625818da1cb64066a3214873
xpwetx <kathrynjmiranda@gmail.com>
2025-11-24 19:13:28 -0800

    Week 2

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -73,5 +73,10 @@ const server = http.createServer((req, res) => {
   }
 });
 
+server.on("request", (req) => {
+  console.log("event received: ", req.method, req.url);
+});
+
+
 server.listen(3000);
 console.log("The server is listening on port 3000.");
